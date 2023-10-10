ca1a13ec2d3d0a3ee54a9bcb84ef64cdb624a575
DwarfFromRoflandia <guckov5555@mail.ru>
2023-10-10 22:34:28 +0300

    Starting creating mechanics to join a room
     - the mechanics of the process of establishing a connection with the server have been completed;
    - a small ui layout;
    - the beginning of the creation of rooms.

--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class PhotonManager : MonoBehaviour
+public class PhotonManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private string _region;
+    [SerializeField] private InputField _roomName;
     public void Intialize()
     {
         ConnectingToServer();
@@ -13,5 +17,29 @@ public class PhotonManager : MonoBehaviour
     private void ConnectingToServer()
     {
         PhotonNetwork.ConnectUsingSettings(); //connecting to the server using file settings
+        PhotonNetwork.ConnectToRegion(_region); //connecting to the required region
+    }
+
+    public override void OnConnectedToMaster() //called when the client is connected to the Master Server and ready for matchmaking and other tasks.
+
+    {
+        Debug.Log($"connected to the region: {PhotonNetwork.CloudRegion}");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) //Called after disconnecting from the Photon server. The reason for this disconnect is provided as DisconnectCause.
+    {
+        Debug.Log("the connection from the server is disconnected");
+    }
+
+    public void CreateRoomButton()
+    {
+        RoomOptions roomOptions = new RoomOptions(); //RoomOptions wraps up common room properties needed when you create rooms.
+        roomOptions.MaxPlayers = 20;
+        PhotonNetwork.CreateRoom(_roomName.text, roomOptions, TypedLobby.Default); //Creates a new room.When successful, this calls the callbacks OnCreatedRoom and OnJoinedRoom (the latter, cause you join as first player). Creating a room will fail if the room name is already in use. 
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log($"room {PhotonNetwork.CurrentRoom.Name} has been created");
     }
 }
