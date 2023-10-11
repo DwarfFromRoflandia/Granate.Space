a4b84061eaeb70be6b824bf4fc316b729b94315d
DwarfFromRoflandia <guckov5555@mail.ru>
2023-10-11 23:01:50 +0300

    Creating and joining a room
    Continuing to create the mechanics of creating and joining a room

--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -24,22 +24,42 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     {
         Debug.Log($"connected to the region: {PhotonNetwork.CloudRegion}");
+        PhotonNetwork.JoinLobby();
     }
 
-    public override void OnDisconnected(DisconnectCause cause) //Called after disconnecting from the Photon server. The reason for this disconnect is provided as DisconnectCause.
+    public override void OnDisconnected(DisconnectCause cause) //called after disconnecting from the Photon server. The reason for this disconnect is provided as DisconnectCause.
     {
         Debug.Log("the connection from the server is disconnected");
     }
 
     public void CreateRoomButton()
     {
-        RoomOptions roomOptions = new RoomOptions(); //RoomOptions wraps up common room properties needed when you create rooms.
+        if (!PhotonNetwork.IsConnected) return; // it will not be possible to create a room if the uder is not connected to Photon servers
+
+        RoomOptions roomOptions = new RoomOptions(); //roomOptions wraps up common room properties needed when you create rooms.
         roomOptions.MaxPlayers = 20;
-        PhotonNetwork.CreateRoom(_roomName.text, roomOptions, TypedLobby.Default); //Creates a new room.When successful, this calls the callbacks OnCreatedRoom and OnJoinedRoom (the latter, cause you join as first player). Creating a room will fail if the room name is already in use. 
+        PhotonNetwork.CreateRoom(_roomName.text, roomOptions, TypedLobby.Default); //creates a new room.When successful, this calls the callbacks OnCreatedRoom and OnJoinedRoom (the latter, cause you join as first player). Creating a room will fail if the room name is already in use. 
+
+        PhotonNetwork.LoadLevel("CallScene"); //switching to another scene when CREATING a room
     }
 
     public override void OnCreatedRoom()
     {
         Debug.Log($"room {PhotonNetwork.CurrentRoom.Name} has been created");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) //called when the server couldn't create a room. The most common cause to fail creating a room, is when a title relies on fixed room-names and the room already exists.
+    {
+        Debug.Log($"Failed to create a room. Perhaps the name of the room {PhotonNetwork.CurrentRoom.Name} already exists");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("CallScene"); //switching to another scene when JOINING a room
+    }
+
+    public void JoinButton()
+    {
+        PhotonNetwork.JoinRoom(_roomName.text);
     }
 }
